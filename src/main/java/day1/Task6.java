0f9288ece894814c7af815564615afc8e0dbeb4c
Dobrinya <dobrij13@gmail.com>
2021-01-20 14:30:28 +0200

    Add Day1 Solution

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -5,7 +5,7 @@ public class Task6 {
 
         int k = 5;
         for (int i=1; i<=9; i++){
-            System.out.println(i + " * "  + k + " = " + i*k);
+            System.out.println(i + "*"  + k + "=" + i*k);
         }
 
     }
