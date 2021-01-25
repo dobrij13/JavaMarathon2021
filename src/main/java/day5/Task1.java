32fd6aa5bd84d9cceb0e75986cc9d8c04c48c2a2
Dobrinya <dobrij13@gmail.com>
2021-01-25 17:18:25 +0200

    Add Day5 Solution

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -1,7 +1,65 @@
 package day5;
+/*
+Создать класс Автомобиль (англ. Car) с полями “Год выпуска”, “Цвет”, “Модель”.
+Создать get и set методы для каждого поля. Создать экземпляр класса Автомобиль.
+Задать значение для каждого поля, используя set методы.
+Вывести в консоль значение каждого из полей, используя get методы.
+Созданный вами класс должен отвечать принципам инкапсуляции.
+
+ */
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car1 = new Car();
+        car1.setYearOfIssue(2015);
+        car1.setColour("Red");
+        car1.setModel("Tesla Model S P85D");
 
+        car1.writeYear();
+        car1.writeColour();
+        car1.writeModel();
+    }
+}
+
+class Car {
+    private int yearOfIssue;
+    private String colour;
+    private String model;
+
+    public void setYearOfIssue(int carYearOfIssue) {
+        yearOfIssue = carYearOfIssue;
+    }
+
+    public int getYearOfIssue() {
+        return yearOfIssue;
+    }
+
+    public void setColour(String carColour) {
+        colour = carColour;
+    }
+
+    public String getColour() {
+        return colour;
+
+    }
+
+    public void setModel(String carModel) {
+        model = carModel;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    void writeYear() {
+        System.out.println("Год выпуска: " + yearOfIssue);
+    }
+
+    void writeColour() {
+        System.out.println("Цвет: " + colour);
+    }
+
+    void writeModel() {
+        System.out.println("Модель: " + model);
     }
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -1,8 +1,36 @@
 package day5;
+/*
+Создать класс Мотоцикл (англ. Motorbike) с полями “Год выпуска”, “Цвет”, “Модель”.
+Создать экземпляр класса Мотоцикл, используя конструктор (set методы не использовать).
+Необходимо придерживаться принципов инкапсуляции и использовать ключевое слово this.
+Вывести в консоль значение каждого из полей, используя get методы.
+ */
 
+//2017,"Black", "Harley-Davidson Street Rod"
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike motorbike1 = new Motorbike(2017,"Black", "Harley-Davidson Street Rod");
+        System.out.println(motorbike1.getYearOfIssue()+"\n"+
+                           motorbike1.getColour()+"\n"+
+                           motorbike1.getModel());
 
     }
+}
+
+class Motorbike {
+    private int yearOfIssue;
+    private String colour;
+    private String model;
+
+    public Motorbike(int yearOfIssue, String colour, String model) {
+        this.yearOfIssue = yearOfIssue;
+        this.colour = colour;
+        this.model = model;
+    }
+
+
+    public int getYearOfIssue() {return yearOfIssue; }
+    public String getColour() { return colour;}
+    public String getModel() {return model;}
 }
